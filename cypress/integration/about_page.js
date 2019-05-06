9d01e4981dac848d3796af65d6cd7105a183cb18
Muqtasid Javaid <muqtasidjavaidkhokhar@gmail.com>
2019-05-06 13:27:35 +0500

    Cypress forms tests added

--- cypress/integration/about_page.js
+++ cypress/integration/about_page.js
@@ -13,8 +13,8 @@ describe('about', function () {
         cy.get('.breadcrumbs > .d-flex > :nth-child(1) > a').contains('Home');
         cy.get('.breadcrumbs > .d-flex > :nth-child(2)').contains('About');
         cy.get('.breadcrumbs > .d-flex > :nth-child(1) > a').contains('Home');
-        // cy.get('.med-history > .container > .row > :nth-child(1) > h2').contains('MedArt');
-        // cy.get('.row > :nth-child(1) > p').contains('laoreet et quam non, viverra');
+        cy.get('.med-history > .container > .row > :nth-child(1) > h2').contains('MedArt');
+        cy.get('.row > :nth-child(1) > p').contains('laoreet et quam non, viverra');
         cy.get('.d-inline-block').contains('Read');
         cy.get('.faq-stuff > .container > :nth-child(1) > :nth-child(1) > h2').contains('Faq');
         cy.get(':nth-child(1) > .professional-box > .d-flex').contains('Professional');
